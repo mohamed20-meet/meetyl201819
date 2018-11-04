bac8616f1418b2bb70ad4815ed51d772362fbfde
mohamed20-meet <mohamed20@meet.mit.edu>
2018-11-04 12:14:09 -0500

    lab4

--- lab3.py
+++ lab3.py
@@ -10,6 +10,7 @@ turtle.forward(100)
 turtle.right(145)
 turtle.forward(100)
 '''
+'''
 turtle.begin_fill()
 turtle.forward(50)
 turtle.right(90)
@@ -22,4 +23,23 @@ turtle.right(50)
 turtle.forward(45)
 turtle.home()
 turtle.end_fill()
+turtle.mainloop()
+'''
+turtle.register_shape("asd.gif")
+turtle.shape("asd.gif")
+dsa = 1
+turtle.speed(100000000000)
+for i in range(720):
+	dsa = dsa + 1
+	turtle.pendown()
+	turtle.right(dsa)
+	turtle.forward(200)
+	
+	turtle.right(55)
+
+	turtle.forward(100)
+	turtle.right(90)
+	turtle.forward(90)
+	turtle.penup()
+	turtle.home()
 turtle.mainloop()
---
+++ lab4.py
@@ -0,0 +1,15 @@
+class Animal(object):
+	def __init__(self, sound, name, age, favorite_food,animal,):
+			self.sound = sound
+			self.name = name
+			self.age = age
+			self.favorite_food = favorite_food 
+			self.animal = animal
+	def eat(self):
+			print(self.animal, "is eating" , self.favorite_food)
+			print("Yummy!!" + self.name + "is eating" )
+	def descriphion(self):
+			print(self.name + "is" + self.age +" years old and loves the color "+self.favorite_color)
+	def sound
+j = Animal("lion", str(20), "photato","reeeeeeeeeeeeeee","dad")
+j.eat()
