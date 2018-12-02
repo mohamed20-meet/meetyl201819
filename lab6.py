2e8000eb8f132af2a112c003d3d6ecfb00a900b2
mohamed20-meet <mohamed20@meet.mit.edu>
2018-12-02 13:03:30 -0500

    lab6.py+lab8.py

--- lab6.py
+++ lab6.py
@@ -1,12 +1,41 @@
-from turtle import *
-class Square(Turtle):
-	def __init__(self,size):
+import turtle
+from turtle import Turtle
+import random
+turtle.colormode(255)
+class Rectangle(Turtle):
+	def __init__ (self,x,y):
 		Turtle.__init__(self)
-		self.size = size
-		self.shapesize(size)
-	def random_color(self,color):
-		Red = 0-256
-		bllue = 0-256
-		green = 0-256
-		print (Square)
+		turtle.register_shape("rectangle",((0,0),(0,y),(x,y),(x,0),(0,0)))
+		self.shape("rectangle")
+	def random_color(self):
+		r = random.randint(0,255)
+		r1 = random.randint(0,255)
+		r2 = random.randint(0,255)
+		self.color([r,r1,r2])
+
+
+# rect = Rectangle(75,100)
+# rect.random_color();
+class Square(Rectangle):
+	def __init__ (self):
+		Rectangle.__init__(self,50,50)
+	
+# Square = Square()
+# Square.random_color()
+
+# class Hexagon(Turtle):
+# 	def __init__(self, x):
+# 		Turtle.__init__(self)
+# 		turtle.register_shape("Hexagon",((0,0),(x,0),(2*x,x),(2*x,2*x),(x,3*x),(0,3*x),(-x,2*x),(-x,x),(0,0)))
+# 		self.shape("Hexagon")
+# 	def random_color(self):
+# 		r = random.randint(0,255)
+# 		r1 = random.randint(0,255)
+# 		r2 = random.randint(0,255)
+# 		self.color([r,r1,r2])
+
+# H = Hexagon(20)
+# H.random_color()
+
+turtle.mainloop()
 
