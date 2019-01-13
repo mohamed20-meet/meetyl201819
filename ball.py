92eb6c2d362a5194a3c929daa1c340b892d873af
mohamed20-meet <mohamed20@meet.mit.edu>
2019-01-13 12:16:26 -0500

    x

---
+++ ball.py
@@ -0,0 +1,4 @@
+import turtle
+
+class BAll(Turtle):
+	def __init__(self, x, y, dx, dy, dy, r, color):
--- my.py
+++ my.py
@@ -13,9 +13,9 @@ class Ball(Turtle):
 		self.shape('circle')
 		self.penup()
 	def move(self,screen_width,screen_height):
-		current_x=self.x
+		current_x=self.xcor()
 		new_x=self.dx+current_x
-		current_y=self.y
+		current_y=self.ycor()
 		new_y=self.dy+current_y
 		right_side_ball=new_x+self.r
 		left_side_ball=new_x-self.r
@@ -24,21 +24,34 @@ class Ball(Turtle):
 		self.goto(new_x,new_y)
 		if right_side_ball>screen_width:
 			self.dx=-self.dx
-		if left_side_ball>screen_width:
+		if left_side_ball< -screen_width:
 			self.dx=-self.dx
 		if up_side_ball>screen_height:
 			self.dy=-self.dy
-		if down_side_ball>screen_height:
+		if down_side_ball< -screen_height:
 			self.dy=-self.dy
+	BALLS = ()
+NUMBER_OF_BALLS=5
+MINIMUM_BALL_RADUIS=10
+MAXIMUM_BALL_RADUIS= 100
+MINIMUM_BALL_DX= -5
+MAXIMUM_BALL_DX=5
+MINIMUM_BALL_DY=-5
+MAXIMUM_BALL_DY= 5
 
 
     
 
 
-	
-		
+my_tuple = (3, 5, 4)
 
-myball=Ball(100,200,50,50,50,"blue")
+random_color = (random.random(), random.random(), random.random())
+screen_width = turtle.getcanvas() .winfo_width()/2
+screen_height = turtle.getcanvas() .winfo_height()/2
+myball=Ball(100,200,50,50,50,random_color)
 myball.move(100,100)
-turtle.mainloop()
-	
+myball2=Ball(100,200,50,50,50,random_color)
+myball2.move(100,100)
+
+while True:
+	myball.move(screen_width,screen_height)
